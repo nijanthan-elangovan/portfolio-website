69ee3adbc2f333f7db038fd2e7dc3b4ca145a4c6
i-code-is-poetry <nijanthan.work@gmail.com>
2025-08-17 17:41:25 +0530

    deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-export default defineConfig({
-  plugins: [react()],
-  // no "base" here – we pass it via CLI script
-});
