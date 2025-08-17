41c359637c7b3bee2ed72613ee8a583e841198a9
i-code-is-poetry <nijanthan.work@gmail.com>
2025-08-17 15:33:43 +0530

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Landing-Page/", // GitHub Pages repo name
+  // Use your new repo name here:
+  base: "/portfolio-website/",
 });
